994281054b7e4d8c6bcdcfac9423a12d24622f14
YashiShashwat <141927115+YashiShashwat@users.noreply.github.com>
2023-11-29 16:55:43 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,10 +1,19 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCaATCwrkY_W-3AJiKSxsQ-eK4ddEQyvEI",
+    authDomain: "copycat1234-e39ca.firebaseapp.com",
+    databaseURL: "https://copycat1234-e39ca.firebaseio.com",
+    projectId: "copycat1234-e39ca",
+    storageBucket: "copycat1234-e39ca.appspot.com",
+    messagingSenderId: "109410559985",
+    appId: "1:109410559985:web:628d0381663c7cb6ff4650"
+  }
 
-//ADD YOUR FIREBASE LINKS HERE
+firebase.initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-    Room_names = childKey;
-   //Start code
+       Room_names = childKey;
+      //Start code
 
-   //End code
-   });});}
+      //End code
+      });});}
 getData();
